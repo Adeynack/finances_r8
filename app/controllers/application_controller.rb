6e0e3e7edc4935f68f6beb353028c5f0ab3104f1
Adeynack <Adeynack@users.noreply.github.com>
2025-04-16 21:58:29 +0000

    remove test route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  def foo
-    FooJob.set(wait: 10.seconds).perform_later
-  end
 end
